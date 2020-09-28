c8cdfbac6274e47ee018a4979e8d95ca76d31926
ErvinRudakov <ervin_rudakov@mail.ru>
2020-09-28 20:42:38 +0200

    Architecture

---
+++ Assets/Scripts/UpdateManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+namespace Game{
+    public class UpdateManager : MonoBehaviour{
+        [SerializeField]
+        private ScriptableEvent _updateEvent;
+        void Update(){
+            _updateEvent.Dispatch();
+        }
+    }
+}
